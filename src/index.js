5bdab44060bdf28245546fccd07d1923b8d69ffd
I524764 <raviganesh.maniyoor@sap.com>
2022-05-31 18:39:50 +0530

    Creating different rooms and chatting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -15,16 +17,31 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
-    socket.emit('message', "Welcome!")
-    socket.broadcast.emit('message', 'A new user has joined') //emit to all other connections except the current socket
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
 
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined.`)) //emit to all other connections except the current socket
 
-    socket.on('sendMessage', (message) => {
-        io.emit('message', message)
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed.')
+        }
+        io.to('Test Room').emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('sendLocation', (location, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.latitude},${location.longitude}`))
+        callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'User has left')
+        io.emit('message', generateMessage('User has left'))
     })
 })
 
